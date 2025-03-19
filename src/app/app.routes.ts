992fb7364ff7259fdc253b1fbb304b0a090aef6d
Cidospark <cidospark27@gmail.com>
2025-03-19 07:11:25 -0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: 'full',
+        component: HomeComponent
+    },
+    {
+        path: "home",
+        loadComponent: ()=>{
+            return import("./pages/home/home.component").then((p)=>p.HomeComponent)
+        }
+    }
+];
